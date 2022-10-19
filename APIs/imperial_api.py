a6c56750280349dc951d39509792fa42e23bfb8d
unknown <petroskitazos@gmail.com>
2022-10-19 23:34:05 +0100

    formatting

--- APIs/imperial_api.py
+++ APIs/imperial_api.py
@@ -1,14 +1,10 @@
 import requests
 import pandas as pd
 
-
-headers = {
-    'Accept': 'application/json'
-}
+headers = {'Accept': 'application/json'}
 
 r = requests.get('https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?dno=10&voltage=HV&start=01-06-2021&end=03-06-2021',
                  params={}, headers=headers)
-
 
 data_dict = r.json()
 data_df = pd.DataFrame(data_dict['data']['data'])
