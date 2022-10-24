ca2f84b6c1d2688b589d294a53faaac811b6cebc
Adam Templeton <2567437T@student.gla.ac.uk>
2022-10-24 19:26:41 +0100

    Added file reader for Uni electricity usage and file it is stored in

--- APIs/UniElectricity/ReadEnergy.py
+++ APIs/UniElectricity/ReadEnergy.py
@@ -1,22 +1,32 @@
-import pandas as 
-import matplotlib.pyplot as plt
-import numpy as np 
-import matplotlib.dates
+import pandas as pd
+import numpy as np
+import json
 
-energy_file = pd.read_excel('UniElec092022.xlsx', sheet_name = 1, usecols="B:C")
+energy_file = pd.read_excel('UniElec092022.xlsx', sheet_name = 1, usecols="D:AY")
 
-energy_list = energy_file.values.tolist()
+#get all the values from first of september records
+time_intervals = energy_file.columns
 
-x = energy_list[:30, 0]
-y = energy_list[:30, 1]
-dates = matplotlib.dates.date2num(x)
+all_first_sep = energy_file.values[::30,]
+num_rows, num_cols = all_first_sep.shape
+avg_first_sep = np.mean(all_first_sep, axis=0)
+
+energy_l = []
 
 
-fig, ax = plt.subplots()
+for i in range(48):
+	temp = {}
+	temp["Timestamp"] = time_intervals[i].strftime("%H:%M")
+	temp["EnergyUsage"] = avg_first_sep[i]
+	energy_l.append(temp)
 
-ax.plot(date, y, linewidth = 2.0)
+energy_d = {}
 
-plt.show()
+energy_d["energyUsage"] = energy_l
+
+with open("../../web-app/src/mock-data/usageData.json", "w") as outfile:
+	json.dump(energy_d, outfile)
+
 
 
 
