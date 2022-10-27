dc9d689c1a047fe6cb0fcd0f7c9a1a5322e28e68
unknown <2526547k@student.gla.ac.uk>
2022-10-27 11:14:25 +0100

    set up new feature branch

---
+++ web-app/src/components/IndividualMeter/IndividualMeter.jsx
@@ -0,0 +1,12 @@
+const IndividualMeter = () => {
+  // logic goes here
+
+  return (
+    <div>
+      {/* component UI goes here */}
+      <div>Hello Adam</div>
+    </div>
+  );
+};
+
+export default IndividualMeter;
