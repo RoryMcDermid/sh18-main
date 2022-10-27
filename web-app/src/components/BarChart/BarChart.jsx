e688cd439ea57214d0971de3dd3f56fe7fb4f1c5
unknown <2526547k@student.gla.ac.uk>
2022-10-27 11:18:24 +0100

    set up new feature branch

---
+++ web-app/src/components/BarChart/BarChart.jsx
@@ -0,0 +1,12 @@
+const BarChart = () => {
+  // logic goes here
+
+  return (
+    <div>
+      {/* component UI goes here */}
+      <div>Hello Boyang</div>
+    </div>
+  );
+};
+
+export default BarChart;
