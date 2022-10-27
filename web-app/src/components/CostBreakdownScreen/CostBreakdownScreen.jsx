4209027010447617aa2f741e4abaac8d194ebd3d
unknown <2526547k@student.gla.ac.uk>
2022-10-27 11:31:13 +0100

    set up new feature branch

---
+++ web-app/src/components/CostBreakdownScreen/CostBreakdownScreen.jsx
@@ -0,0 +1,12 @@
+const CostBreakdownScreen = () => {
+  // logic goes here
+
+  return (
+    <div>
+      {/* component UI goes here */}
+      <div>Hello Ash</div>
+    </div>
+  );
+};
+
+export default CostBreakdownScreen;
