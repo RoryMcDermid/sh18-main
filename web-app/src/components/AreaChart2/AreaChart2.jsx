1d7ae57e54825980be502c6bdd81a80e5ae90b1f
unknown <2526547k@student.gla.ac.uk>
2022-10-27 11:37:54 +0100

    set up new feature branch

---
+++ web-app/src/components/AreaChart2/AreaChart2.jsx
@@ -0,0 +1,12 @@
+const AreaChart2 = () => {
+  // logic goes here
+
+  return (
+    <div>
+      {/* component UI goes here */}
+      <div>Hello Ash</div>
+    </div>
+  );
+};
+
+export default AreaChart2;
