d8febc9905d28ea37b2bbd9d37f5d24a515dc141
unknown <2526547k@student.gla.ac.uk>
2022-10-29 16:31:55 +0100

    basic selection menu implemented

--- web-app/src/components/CompareScreen/CompareScreen.jsx
+++ web-app/src/components/CompareScreen/CompareScreen.jsx
@@ -1,10 +1,77 @@
+import { useState } from "react";
+import { ReactComponent as CloseIcon } from "../../assets/close.svg";
+import energyData from "../../data/energyData.json";
+import EnergyAreaChart from "../AreaChart/EnergyAreaChart";
+
 const CompareScreen = () => {
-  // logic goes here
+  let mockData = [];
+  for (let j = 0; j < 22; j++) {
+    mockData.push({
+      id: `machine ${j + 1}`,
+      data: energyData.data,
+    });
+  }
+
+  const [selectedData, setSelectedData] = useState([]);
+
+  const containsObject = (list, obj) => {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].id === obj.id) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const handleSelect = (newItem) => {
+    if (containsObject(selectedData, newItem)) return;
+    setSelectedData((oldData) => [...oldData, newItem]);
+  };
+
+  const handleClose = (item) => {
+    setSelectedData((oldData) => {
+      return oldData.filter((oldItem) => {
+        return item.id !== oldItem.id;
+      });
+    });
+  };
 
   return (
     <>
-      {/* component UI goes here */}
-      <div>hey P</div>
+      <div className='w-full h-screen grid grid-cols-5'>
+        <div className='col-span-4 flex items-center'>
+          <EnergyAreaChart />
+        </div>
+        <div className='overflow-auto'>
+          <div className='col-span-1 bg-gray-800'>
+            {selectedData.map((item) => (
+              <div
+                key={item.id}
+                className={`m-2 p-3 flex justify-between items-center
+                rounded-lg text-white border-2 border-gray-200/60 cursor-pointer
+                hover:font-semibold hover:border-red-700 hover:text-red-500`}
+                onClick={() => handleClose(item)}
+              >
+                <p>{item.id}</p>
+                <CloseIcon className='h-5 w-5' />
+              </div>
+            ))}
+          </div>
+          <div className='bg-gray-200/60'>&nbsp;</div>
+          <div className='col-span-1 bg-gray-800'>
+            {mockData.map((item) => (
+              <div
+                key={item.id}
+                className='m-2 p-3 rounded-lg text-white hover:bg-gray-200/60 hover:font-semibold cursor-pointer'
+                onClick={() => handleSelect(item)}
+              >
+                {item.id}
+              </div>
+            ))}
+          </div>
+          <div className='bg-gray-200/60'>&nbsp;</div>
+        </div>
+      </div>
     </>
   );
 };
