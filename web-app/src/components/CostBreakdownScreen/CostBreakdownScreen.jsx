86f15d0278da4fd06d5507b79cb248ba1f4a3e23
Adam Templeton <2567437T@student.gla.ac.uk>
2022-10-30 15:19:39 +0000

    New python file in data folder to create cost json file, page displays cost on 24hr scale, still as area chart

--- web-app/src/components/CostBreakdownScreen/CostBreakdownScreen.jsx
+++ web-app/src/components/CostBreakdownScreen/CostBreakdownScreen.jsx
@@ -1,10 +1,48 @@
+import {
+  ResponsiveContainer,
+  AreaChart,
+  XAxis,
+  YAxis,
+  Area,
+  Tooltip,
+  CartesianGrid,
+} from "recharts";
+import CustomTooltip from "../AreaChart/CustomTooltip";
+import costData from "../../data/CostData.json";
+import "../AreaChart/headers.css";
+
+const data = costData.CostData;
+
 const CostBreakdownScreen = () => {
   // logic goes here
 
   return (
-    <div>
-      {/* component UI goes here */}
-      <div>Hello Ash</div>
+    <div className='mx-5'>
+      <h1 class='headers'>24 hour Hourly Cost Data</h1>
+
+      <ResponsiveContainer width='100%' height={500}>
+        <AreaChart data={data}>
+          <defs>
+            <linearGradient id='color' x1='0' y1='0' x2='0' y2='1'>
+              <stop offset='0%' stopColor='#ff6600' stopOpacity={0.5} />
+              <stop offset='75%' stopColor='#ff6600' stopOpacity={0.07} />
+            </linearGradient>
+          </defs>
+          <Area dataKey='Cost' stroke='#ff6600' fill='url(#color)' />
+
+          <XAxis dataKey='Timestamp' />
+
+          <YAxis
+            dataKey='Cost'
+            tickCount={10}
+            tickFormatter={(price) => `£${price.toFixed(2)}`}
+          />
+
+          <Tooltip content={<CustomTooltip symbol='£' />} />
+
+          <CartesianGrid opacity={0.1} vertical={false} />
+        </AreaChart>
+      </ResponsiveContainer>
     </div>
   );
 };
