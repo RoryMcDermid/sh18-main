3ccf4d6d9e17832de9e512f0fc54913946a1b5fc
unknown <2526547k@student.gla.ac.uk>
2022-11-02 09:49:19 +0000

    prepared for merge request

---
+++ web-app/src/App-newCharts.jsx
@@ -0,0 +1,11 @@
+import PriceAreaChart2 from "./components/AreaChart2/PriceAreaChart2";
+
+function App() {
+  return (
+    <div>
+      <PriceAreaChart2 />
+    </div>
+  );
+}
+
+export default App;
