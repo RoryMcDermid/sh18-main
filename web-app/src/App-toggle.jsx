9f6af647329bb56f10d18adcad03df68cd883f39
unknown <2526547k@student.gla.ac.uk>
2022-11-02 09:48:02 +0000

    prepared for merge request

---
+++ web-app/src/App-toggle.jsx
@@ -0,0 +1,11 @@
+import ToggleScreen from "./components/ToggleScreen/ToggleScreen";
+
+function App() {
+  return (
+    <>
+      <ToggleScreen />
+    </>
+  );
+}
+
+export default App;
--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -1,42 +1,26 @@
-import PriceBarChart from "./components/BarChart/PriceBarChart.jsx";
-import PriceAreaChart from "./components/AreaChart/PriceAreaChart.jsx";
-import EnergyBarChart from "./components/BarChart/EnergyBarChart.jsx";
-import EnergyAreaChart from "./components/AreaChart/EnergyAreaChart.jsx";
+import PriceAreaChart from "./components/PriceAreaChart";
+import EnergyAreaChart from "./components/EnergyAreaChart";
 import { useState } from "react";
 
 function App() {
-  const [currentChartData, setCurrentChartData] = useState(true);
-  const [currentChartType, setCurrentChartType] = useState(true);
+  const [currentChart, setCurrentChart] = useState(true);
   return (
     <>
       <div className='mt-5'>
-        {currentChartData && currentChartType && <EnergyAreaChart />}
-        {currentChartData && !currentChartType && <EnergyBarChart />}
-        {!currentChartData && currentChartType && <PriceAreaChart />}
-        {!currentChartData && !currentChartType && <PriceBarChart />}
-
+        {currentChart && <EnergyAreaChart />}
+        {!currentChart && <PriceAreaChart />}
       </div>
-
       <div className='mt-10 w-full flex justify-center gap-5'>
         <div
           className={`px-4 py-3 bg-gray-400 hover:bg-gray-300 rounded-xl
           text-lg font-semibold cursor-pointer`}
-          onClick={() => {setCurrentChartData(!currentChartData)}}
+          onClick={() => setCurrentChart(!currentChart)}
         >
-          {currentChartData ? "Price" : "Energy"}
-
+          {currentChart ? "Price" : "Energy"}
         </div>
-          <div className={`px-4 py-3 bg-gray-400 hover:bg-gray-300 rounded-xl
-          text-lg font-semibold cursor-pointer`}
-              onClick={() => {setCurrentChartType(!currentChartType)}}>
-
-              {currentChartType ? "BarChart" : "AreaChart"}
-          </div>
       </div>
     </>
   );
 }
 
-
 export default App;
-
