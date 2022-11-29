0f63967b871440d7d09eee7e777f34f8362fa5ab
Ashleen <2552805d@student.gla.ac.uk>
2022-11-29 22:22:56 +0000

    Added api calls to retrieve systems data

--- server/server.js
+++ server/server.js
@@ -1,8 +1,38 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql');
 
-app.get("/api", (req, res) => {
-    res.json({"test": "my test data"})
-})
+// Create connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: "root",
+    password: "password",
+    database: "moxie_energy"
+});
+
+// Connect
+db.connect((error) => {
+    if (error) throw error;
+    console.log('Connected to MySql moxie_energy...')
+});
+
+// Get all systems data
+app.get("/getsystems", (req, res) => {
+    let sql = 'SELECT * FROM systems';
+    let query = db.query(sql, (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    })
+});
+
+// Get single system data
+app.get("/getsystems/:id", (req, res) => {
+    let sql = `SELECT * FROM systems WHERE SYSTEM_ID = ${req.params.id}`;
+    let query = db.query(sql, (error, result) => {
+        if (error) throw error;
+        res.send(result);
+    })
+});
+
 
 app.listen(5000, () => {console.log("Server started on port 5000...")})
