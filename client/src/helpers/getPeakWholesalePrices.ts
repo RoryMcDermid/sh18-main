f95d9b654192f556434adc907940d7685554a0f2
Ashleen <2552805d@student.gla.ac.uk>
2023-01-17 22:15:28 +0000

    added helper function to return an array of tuples that contain the peak energy price time slots

---
+++ client/src/helpers/getPeakWholesalePrices.ts
@@ -0,0 +1,41 @@
+import { time } from "console";
+
+export const findAveragePrice = (wholesaleprice: any[]) => {
+  let totalPrice = 0;
+  function addToTotal(pricePoint: any) {
+    totalPrice += pricePoint["Overall"];
+  }
+  wholesaleprice.forEach(addToTotal);
+
+  let totalPricePoints = wholesaleprice.length;
+
+  return Math.ceil(totalPrice / totalPricePoints);
+};
+
+export const getPeakWholesalePrices = (wholesaleprice: any[]) => {
+  let averagePrice = findAveragePrice(wholesaleprice);
+  let filterwholesaleprice = wholesaleprice.map((item) => {
+    if (item.Overall > averagePrice) {
+      return item.Timestamp;
+    }
+  });
+  let expensiveTimes: any[] = [];
+  let timeslot = Array(2);
+  for (let i = 0; i < filterwholesaleprice.length; i++) {
+    if (
+      filterwholesaleprice[i] != undefined &&
+      filterwholesaleprice[i - 1] === undefined
+    ) {
+      timeslot[0] = filterwholesaleprice[i];
+    }
+    if (
+      filterwholesaleprice[i] != undefined &&
+      filterwholesaleprice[i + 1] === undefined
+    ) {
+      timeslot[1] = filterwholesaleprice[i];
+      expensiveTimes.push(timeslot);
+      timeslot = Array(2);
+    }
+  }
+  return expensiveTimes;
+};
--- client/src/screens/SystemSelectScreen.tsx
+++ client/src/screens/SystemSelectScreen.tsx
@@ -2,12 +2,18 @@ import { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Dropdown, Header } from "../components";
 import { formatDropdownData } from "../helpers";
+import {
+  findAveragePrice,
+  getPeakWholesalePrices,
+} from "../helpers/getPeakWholesalePrices";
 import { loadSystems } from "../hooks";
 import { loadWholesalePrice } from "../hooks/loadWholesalePrice";
 
 const SystemSelectScreen: FC = () => {
   const sensorArray = loadSystems();
   const wholesaleprice = loadWholesalePrice("10-01-2023", "16-01-2023");
+  const peaktimes = getPeakWholesalePrices(wholesaleprice);
+  console.log(peaktimes);
 
   const [selectedSystem, setSelectedSystem] = useState<string>("");
   const disable = !(selectedSystem !== "");
