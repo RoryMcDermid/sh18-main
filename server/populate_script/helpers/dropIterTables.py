93625930af39e6f070059f5022158bdb8f25e004
Petros Kitazos <2526547k@student.gla.ac.uk>
2023-02-08 13:17:02 +0000

    removed old populate_script folder

--- server/populate_script/helpers/dropIterTables.py
+++
@@ -1,7 +0,0 @@
-def dropIterTables(sensor_id, mydb, cursor):
-    iter_vals = ["ITER_1", "ITER_2", "ITER_3", "ITER_4"]
-
-    for iter_val in iter_vals:
-        sql =f'''DROP TABLE {iter_val}_{sensor_id}'''
-        cursor.execute(sql)
-    mydb.commit()
