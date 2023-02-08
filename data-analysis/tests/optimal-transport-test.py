44ad25a321da2308d1c730b0f6c2dd76b25eeba2
Petros Kitazos <2526547k@student.gla.ac.uk>
2023-02-08 13:43:41 +0000

    extracted functions into own files, and changed naming to be more consistent throughout project

---
+++ data-analysis/tests/optimal-transport-test.py
@@ -0,0 +1,31 @@
+import numpy as np
+from functions.kMeans import k_means
+from functions.createCostMatrix import create_cost_matrix
+from functions.normalise import normalise_to_probability_distribution
+from functions.sinkhorn import sinkhorn
+from plotters.createHeatMap import create_heatmap
+
+# The goal of the optimal transport algorithm is to find the mapping that
+# transforms the source distribution into the target distribution with the
+# minimum amount of cost
+
+def optimal_transport_test():
+    # test source array to be passed through algorithm
+    source = np.array([1, 10, 6, 9, 2, 4, 4, 1, 1, 7, 8, 9, 2, 3, 4, 5])
+
+    centroids, clusters = k_means(source, 2)
+
+    # mock target where expensive region is near the start 
+    target = [*clusters[np.argmax(centroids)], *clusters[np.argmin(centroids)]]
+
+
+    normalised_source = normalise_to_probability_distribution(source)
+    normalised_target = normalise_to_probability_distribution(target)
+
+    M = create_cost_matrix(normalised_source, normalised_target)
+
+    heatmap_data = sinkhorn(normalised_source, normalised_target, M)
+
+    create_heatmap(heatmap_data)
+
+
