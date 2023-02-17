10825c43dc09f5f7fe82e0c2458c76d358755678
Boyang <2455849l@student.gla.ac.uk>
2023-02-17 13:06:11 +0000

    changed data-analysis folder to backend

--- backend/functions/formatPlotData.py
+++ backend/functions/formatPlotData.py
@@ -4,9 +4,6 @@ import numpy as np
 # turns (96,) 2d array into [(isoTime, reading),(isoTime, reading),...(isoTime, reading)]
 # time will be today's real time, starting from 00:00
 
-# test with 96 zero readings
-test = np.zeros(96)
-
 
 def formatPlotData(dataset) -> np.array:
     start_time = datetime.datetime.now(tz=datetime.timezone.utc).replace(hour=0, minute=0, second=0, microsecond=0)
@@ -14,11 +11,6 @@ def formatPlotData(dataset) -> np.array:
     data = np.zeros(96)
     time_list = [time.isoformat() for time in time_points]
     data_list = [float(value) for value in data]
-
-    # combine time and data together (could be separate, discuss later :) )
     chart_data = list(zip(time_list, data_list))
 
-    print(chart_data)
-
-
-formatPlotData(test)
+    return chart_data
