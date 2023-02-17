9a1663a5676c9d56ed1a73f021957723ed852c29
Petros Kitazos <2526547k@student.gla.ac.uk>
2023-02-17 13:46:45 +0000

    changed input response type from dict to list, and adjusted function accordingly

--- backend/utils/for_chart_data/formatToChartData.py
+++ backend/utils/for_chart_data/formatToChartData.py
@@ -1,12 +1,25 @@
 from typing import List
 
 
-def format_to_chart_data(response: List[List[dict]]) -> List[List]:
+"""
+[
+    [
+        [timestamp_1, value_1], 
+        [timestamp_2, value_2]
+    ],
+    [
+        [timestamp_1, value_1b], 
+        [timestamp_2, value_2b]
+    ]
+]
+"""
+
+def format_to_chart_data(response: List[List[List]]) -> List[List]:
     formatted_data = []
 
     for rows in zip(*response):
-        timestamp = rows[0]["DATE_OF_RECORD"]
-        values = [item["VALUE"] for item in rows]
+        timestamp = rows[0][0]
+        values = [item[1] for item in rows]
         formatted_data.append([timestamp, *values])
 
     return formatted_data
