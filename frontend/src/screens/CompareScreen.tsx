e7fe32fd983485102669271de52fc4dd2ec82a66
Petros Kitazos <2526547k@student.gla.ac.uk>
2023-02-27 14:51:12 +0000

    changed CSS in CompareScreen

--- frontend/src/screens/CompareScreen.tsx
+++ frontend/src/screens/CompareScreen.tsx
@@ -10,14 +10,13 @@ import {
 import { BarChart, MultiLineChart } from "../components";
 import { getValidIntervals } from "../helpers";
 import { loadSensorReadingData, loadSystems, useSensors } from "../hooks";
-import Loading from 'react-loading';
+import Loading from "react-loading";
 
 interface props {
   peakPriceTimes: string[][];
 }
 
 const CompareScreen: FC<props> = ({ peakPriceTimes }) => {
-
   const [formSelection, setFormSelection] = useState({
     selectedSensors: [] as string[],
     startDate: "",
@@ -48,8 +47,6 @@ const CompareScreen: FC<props> = ({ peakPriceTimes }) => {
     interval <= 4
   );
 
-
-
   useEffect(() => {
     console.table(formSelection);
     setFormSelection({
@@ -67,84 +64,94 @@ const CompareScreen: FC<props> = ({ peakPriceTimes }) => {
     }
   }, [sensorReading]);
 
-
   return (
-      <>
-        <div style={{ display: "flex", height: "97vh", width: "97vw"  }}>
-          <div style={{ width: "65%" , marginRight: "30px", display: "flex", height: "72%"}}>
-            {chartReady ? (
-                <>
-                  {currentChartType ? (
-                      <MultiLineChart
-                          headerRow={["", ...selectedSensors]}
-                          data={sensorReading}
-                          peakPriceTimes={peakPriceTimes}
-                      />) : (
-                          <BarChart
-                              headerRow={["", ...selectedSensors]}
-                              data={sensorReading}
-                              peakPriceTimes={peakPriceTimes}
-                          />
-                  )}
-                </>
-            ) : (<div className='w-full' style={{display: "flex", alignItems: "center", justifyContent: "center", height: "100%"}}>
-                  <Loading type="spin" color="#ffffff" height={50} width={50}/>
+    <>
+      <div className='flex h-[97vh]'>
+        <div className='mr-[30px] flex h-[72%] w-[65%]'>
+          {chartReady ? (
+            <>
+              {currentChartType ? (
+                <MultiLineChart
+                  headerRow={["", ...selectedSensors]}
+                  data={sensorReading}
+                  peakPriceTimes={peakPriceTimes}
+                />
+              ) : (
+                <BarChart
+                  headerRow={["", ...selectedSensors]}
+                  data={sensorReading}
+                  peakPriceTimes={peakPriceTimes}
+                />
+              )}
+            </>
+          ) : (
+            <div className='w-full h-full flex justify-center items-center'>
+              <Loading type='spin' color='#ffffff' height={50} width={50} />
             </div>
-            )}
-          </div>
-          <div style={{ width: "30%", height: "100%"}}>
-            <div className='flex flex-col gap-5 w-[33rem]'>
-              <Dropdown
+          )}
+        </div>
+        <div className='w-[30%] h-full'>
+          <div className='flex flex-col gap-5 w-[33rem]'>
+            <Dropdown
               label='Select a system:'
               options={systems}
               onChange={(item) => setSelectedSystem(item)}
               className='w-full'
               getLabel={(system) => system.SYSTEM_NAME}
-              />
-              <MultiSelectDropdown
-                  label='Select sensors:'
-                  items={sensors}
-                  state={selectedSensors}
-                  setState={(e) =>
-                      setFormSelection({...formSelection, selectedSensors: e,})
+            />
+            <MultiSelectDropdown
+              label='Select sensors:'
+              items={sensors}
+              state={selectedSensors}
+              setState={(e) =>
+                setFormSelection({ ...formSelection, selectedSensors: e })
               }
-                  className='w-full'
-              />
+              className='w-full'
+            />
 
-              <div className='flex gap-10'>
-                <DatePicker
-                    label='Select a start date:'
-                    state={startDate}
-                    setState={(e) =>
-                        setFormSelection({...formSelection, startDate: e.target.value})}
-                />
-                <DatePicker
-                    label='Select an end date:'
-                    state={endDate}
-                    setState={(e) =>
-                        setFormSelection({...formSelection, endDate: e.target.value,})}
-                />
-              </div>
-              <ButtonGroup
-                  label='Select sensor reading interval:'
-                  items={["15m", "1h", "4h", "1d"]}
-                  handleSelection={(i) =>
-                      setFormSelection({...formSelection, interval: i,})}
-                  disableItems={getValidIntervals(startDate, endDate)}
+            <div className='flex gap-10'>
+              <DatePicker
+                label='Select a start date:'
+                state={startDate}
+                setState={(e) =>
+                  setFormSelection({
+                    ...formSelection,
+                    startDate: e.target.value,
+                  })
+                }
               />
-              <div className="flex justify-between">
-                <Button
-                    text={currentChartType ? "Bar Chart" : "Line Chart"}
-                    handleClick={() => setCurrentChartType(!currentChartType)}
-                />
-                <Link to='/compare'>
-                  <Button text='Enter' isDisabled={disableButton} />
-                </Link>
-              </div>
+              <DatePicker
+                label='Select an end date:'
+                state={endDate}
+                setState={(e) =>
+                  setFormSelection({
+                    ...formSelection,
+                    endDate: e.target.value,
+                  })
+                }
+              />
+            </div>
+            <ButtonGroup
+              label='Select sensor reading interval:'
+              items={["15m", "1h", "4h", "1d"]}
+              handleSelection={(i) =>
+                setFormSelection({ ...formSelection, interval: i })
+              }
+              disableItems={getValidIntervals(startDate, endDate)}
+            />
+            <div className='flex justify-between'>
+              <Button
+                text={currentChartType ? "Bar Chart" : "Line Chart"}
+                handleClick={() => setCurrentChartType(!currentChartType)}
+              />
+              <Link to='/compare'>
+                <Button text='Enter' isDisabled={disableButton} />
+              </Link>
             </div>
           </div>
         </div>
-      </>
+      </div>
+    </>
   );
 };
 
