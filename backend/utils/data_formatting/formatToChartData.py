417754729598ce266eccd6972322d8ca2fb25fee
Petros Kitazos <2526547k@student.gla.ac.uk>
2023-03-01 16:27:09 +0000

    changed function parameter to *args in function_to_chart_data

--- backend/utils/data_formatting/formatToChartData.py
+++ backend/utils/data_formatting/formatToChartData.py
@@ -2,24 +2,10 @@ from typing import List
 import datetime as dt
 
 
-"""
-[
-    [
-        [timestamp_1, value_1], 
-        [timestamp_2, value_2]
-    ],
-    [
-        [timestamp_1, value_1b], 
-        [timestamp_2, value_2b]
-    ]
-]
-"""
-
-
-def format_to_chart_data(response: List[List[List]]) -> List[List]:
+def format_to_chart_data(*response: List[List[List]]) -> List[List]:
     formatted_data = []
 
-    for rows in zip(*response):
+    for rows in zip(response):
         timestamp = dt.datetime.combine(rows[0][0], rows[0][1])
         values = [item[2] for item in rows]
         formatted_data.append([timestamp, *values])
