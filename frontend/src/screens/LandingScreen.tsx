c8c8826c8c985e4d4d2ff7bc3965a8717fde3a34
Petros Kitazos <2526547k@student.gla.ac.uk>
2023-03-01 13:34:41 +0000

    adjusted landing screen layout

--- frontend/src/screens/LandingScreen.tsx
+++ frontend/src/screens/LandingScreen.tsx
@@ -1,38 +1,33 @@
 import { FC } from "react";
 import React from "react";
-import { Button } from "../components/Inputs"
+import { Button } from "../components/Inputs";
 import { Link } from "react-router-dom";
 
 const LandingScreen: FC = () => {
-
   return (
     <div>
-      <div className="squares flex justify-between w-full mx-auto">
-          <div className='border-2 border-white w-2/5 h-96 m-20'>
-              <div className='mb-4 h-16 flex justify-center text-center items-center text-xl text-white font-semibold'>
-                  Most Expensive Sensors
-              </div>
+      <div className='squares w-full mx-auto grid grid-cols-2 gap-10'>
+        <div className='col-span-1 flex flex-col items-center'>
+          <div className='border-2 border-white w-1/2 h-96 m-20'>
+            <div className='mb-4 h-16 flex justify-center text-center items-center text-xl text-white font-semibold'>
+              Most Expensive Sensors
+            </div>
           </div>
-          <div className='border-2 border-white w-2/5 h-96 m-20'>
-              <div className='mb-4 h-16 flex justify-center text-center items-center text-xl text-white font-semibold'>
-                  Most Expensive Smart Meters
-              </div>
-          </div>
-      </div>
-        <div className="flex justify-between w-full mx-auto">
-            <Link to="/compare">
-                <Button
-                    text="Compare Screen"
-                    className="w-40 ml-80 "
-                />
-            </Link>
-            <Link to="/forecast">
-                <Button
-                    text="Forecast Screen"
-                    className="w-40 mr-80"
-                />
-            </Link>
+          <Link to='/compare'>
+            <Button text='Compare Screen' className='w-40 ' />
+          </Link>
         </div>
+        <div className='col-span-1 flex flex-col items-center'>
+          <div className='border-2 border-white w-1/2 h-96 m-20'>
+            <div className='mb-4 h-16 flex justify-center text-center items-center text-xl text-white font-semibold'>
+              Most Expensive Smart Meters
+            </div>
+          </div>
+          <Link to='/forecast'>
+            <Button text='Forecast Screen' className='w-40' />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
