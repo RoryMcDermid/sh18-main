fa3ba72a00d2843c3be8c9e0dd3cc0b1ee524260
Boyang <2455849l@student.gla.ac.uk>
2023-03-01 12:39:40 +0000

    Landing page added, findings page renamed forecast page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import { Header } from "./components";
-import { CompareScreen } from "./screens";
-import FindingsScreen from "./screens/FindingsScreen";
+import { CompareScreen} from "./screens";
+import ForecastScreen from "./screens/ForecastScreen";
+import LandingScreen from "./screens/LandingScreen";
+
 
 function App() {
   return (
-    <>
+      <>
       <Header />
-      <Routes>
-        <Route path='/compare' element={<CompareScreen />} />
-        <Route path='/findings' element={<FindingsScreen />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<LandingScreen />} />
+          <Route path='/landing' element={<LandingScreen />} />
+          <Route path='/compare' element={<CompareScreen />} />
+          <Route path='/forecast' element={<ForecastScreen />} />
+        </Routes>
     </>
   );
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Header: FC = () => {
   return (
     <>
-      <header className='mx-20 h-[15vh] flex justify-start items-center'>
-        <Link to='/'>
+      <header className='mx-20 h-[15vh] flex justify-center items-center'>
+        <Link to='/landing'>
           <div className='text-4xl text-white text-center'>Moxie Energy</div>
         </Link>
       </header>
